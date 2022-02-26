830ced0300750f6d8d3fdc3ad8e6a7c501905506
jerremyng <jerrremyng@gmail.com>
2022-02-26 22:25:20 +0800

    updated code

---
+++ deploy_filter.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import numpy as np
+import cv2
+import mediapipe as mp
+import os
+import pyvirtualcam
+
+from tensorflow.keras.models import load_model
+
+# actions = pd.read_csv(os.path.join('output_files','actions.csv')).iloc[:,1]
+label_map = pd.read_csv(os.path.join('model_data',"label_map.csv")).columns[1:].tolist()
+model = load_model(os.path.join('model_data','ASL_model.h5'))
+
+mp_holistic = mp.solutions.holistic # keypoint model
+mp_drawing = mp.solutions.drawing_utils # drawing functions
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+def draw_styled_landmarks(image, results):
+    # Draw face connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS, 
+                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                             ) 
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                             ) 
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                             ) 
+    # Draw right hand connections  
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                             ) 
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, face, lh, rh])
+
+# 1. New detection variables
+sequence = []
+sentence = []
+predictions = []
+threshold = 0.5
+
+print ('Starting virtual camera')
+cap = cv2.VideoCapture(0)
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    with pyvirtualcam.Camera(width=1280, height=720, fps=30,device='OBS Virtual Camera') as cam:
+        try:
+            while cap.isOpened():
+
+                # Read feed
+                ret, frame = cap.read()
+
+                # Make detections
+                image, results = mediapipe_detection(frame, holistic)
+                # print(results)
+                
+                # Draw landmarks
+                draw_styled_landmarks(image, results)
+                
+                # 2. Prediction logic
+                keypoints = extract_keypoints(results)
+                sequence.append(keypoints)
+                sequence = sequence[-134:]
+                
+                if len(sequence) == 134:
+                    res = model.predict(np.expand_dims(sequence, axis=0))[0]
+                    print(label_map[np.argmax(res)])
+                    predictions.append(np.argmax(res))
+                    
+                    
+                #3. Viz logic
+                    if np.unique(predictions[-10:])[0]==np.argmax(res): 
+                        if res[np.argmax(res)] > threshold: 
+                            
+                            if len(sentence) > 0: 
+                                if label_map[np.argmax(res)] != sentence[-1]:
+                                    sentence.append(label_map[np.argmax(res)])
+                            else:
+                                sentence.append(label_map[np.argmax(res)])
+
+                    if len(sentence) > 5: 
+                        sentence = sentence[-5:]
+
+                    # Viz probabilities
+                    # image = prob_viz(res, actions, image, colors)
+                    
+                cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+                cv2.putText(image, ' '.join(sentence), (3,30), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                
+                # Show to screen
+                # cv2.imshow('OpenCV Feed', image)
+                image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+                cam.send(image)
+
+                # Break gracefully
+        except KeyboardInterrupt:
+                cap.release()
+                # cv2.destroyAllWindows()
